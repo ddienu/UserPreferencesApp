2cf82c633ab772e598e79bc630219439bb409121
ddienu <rockdann@hotmail.com>
2022-06-18 20:15:05 -0500

    Update: Settings page
    Update from Stateless to Statefull widget.
    Update the settings page with three private variables to control the application, each one manage the page.
    Add a ListView with radiolisttile with its setstate respectly.

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -2,10 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:userpreferencesapp/src/widgets/menu_widget.dart';
 
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   static final String routeName = 'settings';
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+
+  TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+  
+    super.initState();
+    _textController = TextEditingController( text: _nombre);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +36,60 @@ class SettingsPage extends StatelessWidget {
         title: const Text('Ajustes'),
       ),
       drawer: MenuWidget(),
-      body: const Center(
-        child: Text('Ajustes Page'),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: Text('Settings', style: TextStyle(fontSize: 45.5, fontWeight: FontWeight.bold),),
+          ),
+          const Divider(),
+          SwitchListTile(
+            value: _colorSecundario, 
+            title: Text('Color secundario'),
+            onChanged: (value){
+              setState(() {
+              _colorSecundario = value;
+              });
+            }
+             ),
+             RadioListTile(
+              value: 1, 
+              title: Text('Masculino'),
+              groupValue: _genero, 
+              onChanged: ( int? value ){
+                setState(() {
+                _genero = value!;
+                  
+                });
+              },
+              ),
+
+              RadioListTile(
+              value: 2, 
+              title: const Text('Femenino'),
+              groupValue: _genero, 
+              onChanged: ( int ?value ){
+                setState(() {
+                _genero = value!;
+                  
+                });
+              },
+              ),
+              const Divider(),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    labelText: 'Nombre',
+                    helperText: 'Nombre de la persona usando el telefono',
+                  ),
+                  onChanged: ( value ){},
+                ),
+              )
+        ],
       )
+      
     );
 }
 }
